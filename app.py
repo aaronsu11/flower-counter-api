a528e5d2d3268a8472a9cc7606af3bbe2c294295
Aaron <aaronsu11@outlook.com>
2019-09-10 17:28:36 +1000

    adding requirements.txt

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask, request, redirect
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         return "Got it!"
         # return redirect('/')
     else:
         return "Hello, World !"
 
 
-@app.route('/home/<int:id>')
+@app.route("/home/<int:id>")
 def home(id):
     return "Home Page"
 
